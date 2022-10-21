3e955386abc15d46ed8bab19b54f6860bec74f33
tima1617 <maeltima@gmail.com>
2022-10-21 15:53:43 +0200

    add change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ import {
   useMapEvents,
   Marker,
   Popup,
+  useMap,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import Leaflet from "leaflet";
@@ -14,9 +15,27 @@ function Home() {
   const position = [47.203, -1.55];
   const position2 = [47.203, -1.535];
 
+  function MyComponent() {
+    const map = useMapEvents({
+      click: () => {
+        map.locate();
+      },
+      locationfound: (location) => {
+        console.log("location found:", location);
+      },
+    });
+    return null;
+  }
+
   return (
     <div className="main">
-      <MapContainer center={[47.203, -1.537]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer
+        id="map"
+        center={[47.203, -1.537]}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <MyComponent></MyComponent>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
