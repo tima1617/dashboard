0db1d2fa102c847c1d8340134d21828355cf85b0
tima1617 <maeltima@gmail.com>
2022-10-21 14:19:19 +0200

    add map

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,19 @@
 import "./Home.css";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 function Home() {
-  return <div className="main"></div>;
+  const position = [51.505, -0.09];
+  return (
+    <div className="main">
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
 }
 
 export default Home;
