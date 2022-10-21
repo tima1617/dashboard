9bac0f72b8b9dba37e89ebfb83ff59d11f9e2fe6
tima1617 <maeltima@gmail.com>
2022-10-21 15:06:55 +0200

    add markers

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,33 +7,32 @@ import {
   Popup,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import icon from "./constants";
-import Leflet from "leaflet";
+import Leaflet from "leaflet";
+import iconMarker from "../components/constants";
 
 function Home() {
-  const position = [51.505, -0.09];
-
-  function MyComponent() {
-    const map = useMapEvents({
-      click: (e) => {
-        const { lat, lng } = e.latlng;
-        Leflet.marker([lat, lng], { icon }).addTo(map);
-      },
-    });
-    return null;
-  }
+  const position = [47.203, -1.55];
+  const position2 = [47.203, -1.535];
 
   return (
     <div className="main">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={[47.203, -1.537]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
+        <Marker position={position} icon={iconMarker}>
           <Popup>
             <center>
               Téléphone portable
+              <br /> James Anderson.
+            </center>
+          </Popup>
+        </Marker>
+        <Marker position={position2} icon={iconMarker}>
+          <Popup>
+            <center>
+              Vidéo projecteur
               <br /> James Anderson.
             </center>
           </Popup>
